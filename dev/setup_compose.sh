92c1e5f24532897295ec80904d11254e388118ab
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-08 01:47:53 +0300

    Added shell script to create data volumes and start compose

---
+++ dev/setup_compose.sh
@@ -0,0 +1,6 @@
+echo "Creating data volumes for Consul and SQL Server..."
+docker volume create --name=climatemeter-sqlserver-data
+docker volume create --name=climatemeter-sqlserver-data
+
+echo "Starting docker-compose..."
+docker-compose up -d
