62eb4831c0a3f074d27bc2bbff6c7f474f476910
Volodymyr Osmak <darkxahtep@gmail.com>
2017-12-01 22:44:31 +0200

    Added docker-compose and start.sh script

---
+++ device/master/start.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+echo "Reading device info..."
+source ../config/device_info.sh
+echo "Device name is: ${DEVICE_DeviceName}"
+
+echo "Creating data volume for Consul and Portainer..."
+docker volume create --name=climatemeter-consul-data
+docker volume create --name=climatemeter-portainer-data
+
+echo "Starting docker-compose..."
+docker-compose up -d
