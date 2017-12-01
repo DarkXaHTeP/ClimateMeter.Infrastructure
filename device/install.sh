bee6946f8b767c7c99d7e3a56fdc1ef673453c2c
Volodymyr Osmak <darkxahtep@gmail.com>
2017-12-01 22:56:34 +0200

    Updated quotes in sed command again

--- device/install.sh
+++ device/install.sh
@@ -3,6 +3,6 @@ echo "Please type the device name and press [Enter]:"
 read DEVICE_DeviceName
 echo "Please type the device description and press [Enter]:"
 read DEVICE_DeviceDescription
-sed -i "s|{name}|${DEVICE_DeviceName}|g" ./config/devive_info.sh
-sed -i "s|{description}|${DEVICE_DeviceDescription}|g" ./config/devive_info.sh
+sed -i 's|{name}|'"${DEVICE_DeviceName}"'|g' ./config/devive_info.sh
+sed -i 's|{description}|'"${DEVICE_DeviceDescription}"'|g' ./config/devive_info.sh
 echo "Wrote name and description values into config/device_info.sh"
